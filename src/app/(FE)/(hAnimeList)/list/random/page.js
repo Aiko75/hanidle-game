fdee50ac925148f4ea4ab7c0fa2c3c27473258b8
Nhan <trannhan07052005@gmail.com>
2025-12-27 14:41:55 +0700

    fix gameList bug

--- src/app/(FE)/(hAnimeList)/list/random/page.js
+++ src/app/(FE)/(hAnimeList)/list/random/page.js
@@ -1,5 +1,12 @@
+import { Suspense } from "react";
 import RandomHAnime from "@/pages/List/randomHAnime/Random";
 
 export default function Page() {
-  return <RandomHAnime />;
+  return (
+    <Suspense
+      fallback={<div className="p-10 text-center">Loading random...</div>}
+    >
+      <RandomHAnime />
+    </Suspense>
+  );
 }
